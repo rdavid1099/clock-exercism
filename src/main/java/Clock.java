403765b033ed117a7a84176e38ccddae0254415c
Ryan Workman <rdavid1099@gmail.com>
2017-12-12 15:25:02 -0700

    Create base foundation for clock creation

--- src/main/java/Clock.java
+++ src/main/java/Clock.java
@@ -1,3 +1,22 @@
 class Clock {
-    
+    private int rawHour, rawMin;
+    private String hour, minute;
+    public Clock(int hour, int min) {
+        this.rawHour = hour;
+        this.rawMin = min;
+    }
+
+    public String toString() {
+        convertRawTime();
+        return hour + ":" + minute;
+    }
+
+    private void convertRawTime() {
+        hour   = formatNum(Integer.toString(rawHour));
+        minute = formatNum(Integer.toString(rawMin));
+    }
+
+    private String formatNum(String rawNum) {
+        return (rawHour < 10) ? "0" + rawNum : rawNum;
+    }
 }
